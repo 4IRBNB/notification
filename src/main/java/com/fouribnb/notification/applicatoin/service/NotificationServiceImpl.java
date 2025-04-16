efa8e2e96611e761804c1b82ef74cc1d6207ca1d
oh-min <ohyemin96@gmail.com>
2025-04-16 14:48:22 +0900

    ♻️ refactor[#3] : 불필요한 주석 제거

--- src/main/java/com/fouribnb/notification/applicatoin/service/NotificationServiceImpl.java
+++ src/main/java/com/fouribnb/notification/applicatoin/service/NotificationServiceImpl.java
@@ -5,7 +5,6 @@ import com.fouribnb.notification.applicatoin.dto.responseDto.NotificationInterna
 import com.fouribnb.notification.applicatoin.mapper.NotificationMapper;
 import com.fouribnb.notification.domain.entity.Notification;
 import com.fouribnb.notification.domain.repository.NotificationRepository;
-import com.fouribnb.notification.infrastructure.jpa.NotificationJpaRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -15,16 +14,12 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class NotificationServiceImpl implements NotificationService {
 
-        private final NotificationRepository notificationRepository;
-//    private final NotificationJpaRepository notificationJpaRepository;
+    private final NotificationRepository notificationRepository;
 
-    public NotificationInternalResponse createNotification(CreateNotificationInternalRequest request) {
-        // 예약 정보를 가져와서  -> 알림 메세지로 조합
+    public NotificationInternalResponse createNotification(
+        CreateNotificationInternalRequest request) {
         Notification notification = NotificationMapper.toEntity(request);
-        log.info("mapper 적용 후 : {}", notification.toString());
-        // DB에 저장
         Notification saved = notificationRepository.save(notification);
-//        Notification saved = notificationJpaRepository.save(notification);
         log.info("DB 저장 : {}", saved);
         return NotificationMapper.toResponse(saved);
     }
