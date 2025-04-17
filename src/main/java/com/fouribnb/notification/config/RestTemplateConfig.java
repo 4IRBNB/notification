9d97777ab535486388c01c64de65e565f5889d76
oh-min <ohyemin96@gmail.com>
2025-04-18 00:44:17 +0900

    ✨ feat[#5] : RestTemplateConfig 생성
    - slackRestTemplate 메서드 추가

---
+++ src/main/java/com/fouribnb/notification/config/RestTemplateConfig.java
@@ -0,0 +1,15 @@
+package com.fouribnb.notification.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestTemplateConfig {
+
+    @Bean
+    public RestTemplate slackRestTemplate() {
+        return new RestTemplate();
+    }
+
+}
